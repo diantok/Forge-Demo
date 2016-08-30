bc2f83f41d6d0b506276f51a6c3f8914fe4b0c2d
Moeghan <diantok@gmail.com>
2016-08-30 13:57:05 +0700

    edit database

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
         'mysql' => array(
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST'),
-            'database'  => env('DB_DATABASE'),
-            'username'  => env('DB_USERNAME'),
-            'password'  => env('DB_PASSWORD'),
+            'host'      => 'localhost',
+            'database'  => 'forge',
+            'username'  => 'forge',
+            'password'  => '2IRYMhvJJb58eQ1DueaK',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
