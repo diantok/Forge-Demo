da245ace984c6a35768a5e58912135cef190358e
Moeghan <diantok@gmail.com>
2016-08-30 15:39:08 +0700

    db environment

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
         'mysql' => array(
             'driver'    => 'mysql',
-            'host'      => 'localhost',
-            'database'  => 'forge',
-            'username'  => 'forge',
-            'password'  => '2IRYMhvJJb58eQ1DueaK',
+            'host'      => env('localhost'),
+            'database'  => env('DB_DATABASE'),
+            'username'  => env('DB_USERNAME'),
+            'password'  => env('DB_PASSWORD'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
